85e119705733a7bfc82460e2c1ac8ac46e304527
louie <louiecasula@gmail.com>
2023-11-11 10:41:28 -0500

    finisheddd

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,115 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    Character[][] matrix;
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        int i, j;
+        // Test winning row
+        for (i = 0; i < matrix.length; i++){
+            for (j = 0; j < matrix.length; j++){
+                if (matrix[i][j] != 'X'){
+                    break;
+                }
+            }
+            if (j == matrix.length){
+                return true;
+            }
+        }
+        // Test winning column
+        for (i = 0; i < matrix.length; i++){
+            for (j = 0; j < matrix.length; j++){
+                if (matrix[j][i] != 'X'){
+                    break;
+                }
+            }
+            if (j == matrix.length){
+                return true;
+            }
+        }
+        // Test winning [0][0] -> [max][max] diagonal
+        for (i = 0; i < matrix.length; i++){
+            if (matrix[i][i] != 'X'){
+                break;
+            }
+            if (i == matrix.length - 1){
+                return true;
+            }
+        }
+        // Test winning [max][0] -> [0][max] diagonal
+        for (i = 0; i < matrix.length; i++){
+            if (matrix[i][matrix.length - i - 1] != 'X'){
+                break;
+            }
+            if (i == matrix.length - 1){
+                return true;
+            }
+        }
+        return false;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        int i, j;
+        // Test winning row
+        for (i = 0; i < matrix.length; i++){
+            for (j = 0; j < matrix.length; j++){
+                if (matrix[i][j] != 'O'){
+                    break;
+                }
+            }
+            if (j == matrix.length){
+                return true;
+            }
+        }
+        // Test winning column
+        for (i = 0; i < matrix.length; i++){
+            for (j = 0; j < matrix.length; j++){
+                if (matrix[j][i] != 'O'){
+                    break;
+                }
+            }
+            if (j == matrix.length){
+                return true;
+            }
+        }
+        // Test winning [0][0] -> [max][max] diagonal
+        for (i = 0; i < matrix.length; i++){
+            if (matrix[i][i] != 'O'){
+                break;
+            }
+            if (i == matrix.length - 1){
+                return true;
+            }
+        }
+        // Test winning [max][0] -> [0][max] diagonal
+        for (i = 0; i < matrix.length; i++){
+            if (matrix[i][matrix.length - i - 1] != 'O'){
+                break;
+            }
+            if (i == matrix.length - 1){
+                return true;
+            }
+        }
+        return false;
     }
 
     public Boolean isTie() {
-        return null;
+        return !isInFavorOfO() && !isInFavorOfX();
     }
 
     public String getWinner() {
-        return null;
+        if (isInFavorOfO()){
+            return "O";
+        }
+        else if (isInFavorOfX()){
+            return "X";
+        }
+        else {
+            return "";
+        }
     }
 
 }
